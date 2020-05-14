fa9a9d7e2f7279a91379ea2363adb2a0f6c51adc
Marcelo Kenji Cataoca <marcelokenjicataoca@gmail.com>
2020-05-14 10:24:04 -0300

    call  middleware chain

--- array/middleware.js
+++ array/middleware.js
@@ -10,3 +10,16 @@ const pass2 = (ctx, next) => {
 }
 
 const pass3 = ctx => ctx.valor3 = 'mdl3'
+
+//using rest operator 
+const test = (ctx, ...middlewares) => {
+    const execute = index => {
+        middlewares && index < middlewares.length &&
+        middlewares[index](ctx, () => execute(index + 1))
+    }
+    execute(0)
+}
+
+const ctx = {}
+test(ctx, pass1, pass2, pass3) 
+console.log(ctx);
