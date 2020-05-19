a727de24820f8229221a4df8f3e417199a0cc8a1
Marcelo Kenji Cataoca <marcelokenjicataoca@gmail.com>
2020-05-18 22:09:01 -0300

    add node section

---
+++ node/project/src/server.js
@@ -0,0 +1,42 @@
+const port = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const database = require('./database')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/products', (req, res, next) => {
+    res.send(database.getProducts())
+})
+
+app.get('/products/:id', (req, res, next) => {
+    res.send(database.getProduct(req.params.id))
+})
+
+app.post('/products', (req, res, next) => {
+    const product = database.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.put('/products:id', (req, res, next) => {
+    const product = database.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.delete('/products:id', (req, res, next) => {
+    const product = database.deleteProduct(req.params.id)
+    return product
+})
+
+app.listen(port, () => {
+    console.log(`Servidor está executando na porta ${port}`);
+})
